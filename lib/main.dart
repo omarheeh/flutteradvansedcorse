0dcf4e4c285b27941872a86e0b481f2107e083fa
omarheeh <codeomar3@gmail.com>
2024-11-19 17:00:50 +0300

    first commint

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: const DocDocApp(),
+    );
+  }
+}
+
+class DocDocApp extends StatelessWidget {
+  const DocDocApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: double.infinity,
+          ),
+          Text(
+            "Omar",
+          )
+        ],
+      ),
+    );
+  }
+}
